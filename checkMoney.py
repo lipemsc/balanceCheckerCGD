c3c09f81aac4620f26b1cfe1402ca5c045f8c3f5
lipe.msc <lipe.msc@gmail.com>
2021-08-13 11:51:44 +0100

    First commit, simple script

---
+++ checkMoney.py
@@ -0,0 +1,20 @@
+
+import requests,json
+from bs4 import BeautifulSoup
+
+f = open("cookies.json", "r")
+
+cookieFile = json.loads(f.read())
+
+cookies={}
+
+for x in cookieFile:
+    cookies[x["Name raw"]] = x["Content raw"]
+
+
+r = requests.get('https://caixadirectaonline.cgd.pt/cdo/private/contasaordem/consultaSaldosMovimentos.seam', cookies=cookies, headers={"user-agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"})
+#print(r.text)
+parsed_html = BeautifulSoup(r.text, features="lxml")
+saldo = parsed_html.body.find('div', attrs={'class':'saldos disponivel'})
+#print(saldo)
+print(saldo.find('p', attrs={'class':'valor'}).label.text)
